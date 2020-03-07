5ca6b1ff6e7069bab27fa50bc9dec876d4d43521
Simon Müller <SimonMueller@users.noreply.github.com>
2020-03-07 15:23:55 +0100

    readd dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+import MessageName from './communication/messageName';
+export { MessageName };
+export { createJokerCard, compareRanks, equals as cardEquals, createSuitCard, Colors, Suits, JOKER_CARD_KIND, SUIT_CARD_KIND, } from './game/card';
+export { Color, Card, JokerCard, JokerRank, Suit, SuitCard, SuitRank, Rank, } from './types/card';
+export { Error } from './types/communication';
+export { Message, BroadcastPlayers, StartGame, BroadcastStartGame, DealtCards, BroadcastPlayerOrder, RequestCards, PlayCards, BroadcastPlayerTurn, BroadcastPlayerTurnError, BroadcastRoundWinner, BroadcastPenalties, BroadcastOutPlayers, BroadcastGameWinner, BroadcastStartRound, BroadcastEndRound, BroadcastStartCycle, BroadcastEndCycle, BroadcastStartPlayerTurn, BroadcastGameError, BroadcastEndGame, AvailableCards, } from './types/message';
+export { PlayerId, OutPlayer } from './types/player';
+export { Penalty } from './types/penalty';
+export { ValidatedTurn, TurnError } from './types/turn';
---
+++ dist/types/communication.d.ts
@@ -0,0 +1,4 @@
+import { TurnError } from './turn';
+export interface Error {
+    readonly error: string | TurnError;
+}
